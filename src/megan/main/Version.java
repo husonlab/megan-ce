a643f3f5fe2c3fc064ef07b04fc3e26208907bd9
danielhuson <daniel@huson.de>
2018-01-25 15:37:52 +0100

    minor fix

--- src/megan/main/Version.java
+++ src/megan/main/Version.java
@@ -27,6 +27,6 @@ package megan.main;
 public class Version {
     static public final String NAME = "MEGAN";
     // this string gets modified by the nightly  build script...
-    static public final String SHORT_DESCRIPTION = "MEGAN Community Edition (version 6.10.5, built 23 Nov 2017)";
+    static public final String SHORT_DESCRIPTION = "MEGAN Community Edition (version 6.10.7, built 25 Jan 2018)";
 }
 
